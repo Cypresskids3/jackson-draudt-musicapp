4cc358b67e821675b4a07e0bf0ec5540ee44f445
Cypresskids3 <134014283+Cypresskids3@users.noreply.github.com>
2024-04-29 18:15:29 -0500

    Add files via upload

---
+++ app/Body.js
@@ -0,0 +1,32 @@
+import styles from './styles.css'
+import MusicBox from './MusicBox'
+export default function Body(){ //Body components
+    return (
+        <div className="body-content">
+        <MusicBox name={"survival"} artist ={"drake"}  album={"scoripion"} source= {"Drake-2048x1365.jpg"} link={"https://www.youtube.com/watch?v=VtEBRE9ul90"} lyric={"there is a whole lot in my possesion"} />
+        <MusicBox name={"ballin"} artist ={"mustard"}  album={"perfect ten"} source= {"Mustard-press-photo-cr-B-Wade-2019-billboard-1548-compressed.jpg"} link={"https://www.youtube.com/watch?v=Ni92kRoMNiA"} lyric={"i put the new forgees on the jeep"} />
+        <MusicBox name={"godzilla"} artist ={"eminem"}  album={"music to be murded by"} source= {"th (1).jpg"} link={" https://www.youtube.com/watch?v=SmBzqkgdH9I"} lyric={"ugh your a monster"} />
+        <MusicBox name={"lucid dreams "} artist ={"juice wrld"}  album={"godbye and good ridence"} source= {"th.jpg"} link={"https://www.youtube.com/watch?v=_fh64GbFSw4"} lyric={"i still see your shadows in my room"} />
+        <MusicBox name={"FE!N  "} artist ={"travis scott"}  album={" birds in the trap sing mcknight"} source= {"th (2).jpg"} link={"https://www.youtube.com/watch?v=hF1sqWiqppE"} lyric={"2:00 AM"} />
+        <MusicBox name={"goosebumps"} artist ={"travis scott"}  album={" birds in the trap sing mcknight"} source= {"th (2).jpg"} link={" https://www.youtube.com/watch?v=_9XaXHD3680"} lyric={"peter piper picked your brain apart and put together a lover"} />
+        <MusicBox name={"the  real slim shady"} artist ={"emenim"}  album={"encore"} source= {"th (1).jpg"} link={" https://www.youtube.com/watch?v=rkqMbsmLrtA"} lyric={"yees im the real slim shady "} />
+        <MusicBox name={"my name is"} artist ={"emenim"}  album={"encore"} source= {"th (1).jpg"} link={" https://www.youtube.com/watch?v=t4V-hlr8134 "} lyric={"my name is chikka chika slim shady"} />
+        <MusicBox name={"whats the difference"} artist ={"dr dre "}  album={"2000"} source= {"Dr.-Dre.jpg"} link={" https://www.youtube.com/watch?v=-Ubyt7iWJ8Q "} lyric={"whats the difference between you and me "} />
+        <MusicBox name={"the next episode"} artist ={"dr dre and snoop dog"}  album={"2000"} source= {"Dr.-Dre.jpg"} link={"https://www.youtube.com/watch?v=A8V0-gLxeB0 "} lyric={"ladadada"} />
+        </div>
+    )
+}
+
+/*
+survival | drake | scoripion | https://www.youtube.com/watch?v=VtEBRE9ul90 | there is a whole lot in my possesion
+ballin | mustard | perfect 10 | https://www.youtube.com/watch?v=Ni92kRoMNiA | in put tyhe new forgees on the geep
+Godzilla | Music to be murded by | https://www.youtube.com/watch?v=SmBzqkgdH9I | ugh your a monster
+lucid dreamns | goodbye and good ridence | https://www.youtube.com/watch?v=_fh64GbFSw4 | i still see your shadows in my room
+FE!N | birds in the trap sing mcknight | https://www.youtube.com/watch?v=hF1sqWiqppE | 2;00 am
+goose bumps |  birds in the trap sing mcknight | https://www.youtube.com/watch?v=_9XaXHD3680 | peter piper picked your brain apart and put together a lover
+the real slim shady | encore | https://www.youtube.com/watch?v=rkqMbsmLrtA | yed im the real slim shady
+my name is | encore | https://www.youtube.com/watch?v=t4V-hlr8134 | my name is chikka chikka slim shady
+whats the difference | 2000 | https://www.youtube.com/watch?v=-Ubyt7iWJ8Q | never forget the ship
+the next episode | 2000 | https://www.youtube.com/watch?v=A8V0-gLxeB0 | ladada
+
+*/
---
+++ app/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar(){ //Navbar components
+    return (
+        <div className="navbar">
+            <img src="logo.png" alt="logo" className="nav-logo"/>
+            <h1 className="nav-Title">too much code :(((</h1>
+            <a href="" className="nav-link">More Info</a>
+        </div>
+    )
+}
